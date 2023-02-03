9973610a47ef54249d42316d3b100cff92fddfd5
benny-china <72130587+benny-china@users.noreply.github.com>
2023-02-03 10:49:15 +0800

    Create popcount.go

---
+++ chapter2/popcount.go
@@ -0,0 +1,24 @@
+package main
+import (
+    "fmt"
+)
+
+var pc[256]byte
+func init() {
+    for i := range pc {
+        pc[i] = pc[i/2] + byte(i&1)
+    }
+}
+
+func PopCount(x uint64) int {
+    num := 0
+    for i:=0;i<8;i++ {
+        num += int(pc[byte(x>>(i*8))])
+    }
+    return num
+}
+
+func main() {
+    count := PopCount(257)
+    fmt.Println(count)
+}
